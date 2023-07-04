8bdbcc0b5d8e6042a6f63ce523bf53431ce84efe
minhvd <minhvd@mirabo-global.com>
2023-07-05 02:23:19 +0700

    fix

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,21 +4,20 @@
       <v-icon left class="mr-2">fas fa-bars</v-icon>
     </v-btn>
     <v-toolbar-title>Danh Mục</v-toolbar-title>
-    <div style="
+    <div
+      style="
         position: absolute;
         margin-left: auto;
         margin-right: auto;
         left: 0;
         right: 0;
         text-align: center;
-      ">
-      <h4>Laptop Store</h4>
+      "
+    >
+      <h4>LAPTOP STORE</h4>
     </div>
     <v-spacer></v-spacer>
-    <v-col cols="3">
-      <v-text-field density="compact" variant="outlined" label="Search" append-inner-icon="fas fa-search" single-line
-        hide-details class="mr-2"></v-text-field>
-    </v-col>
+    <v-col cols="3"> </v-col>
   </v-app-bar>
 </template>
 <script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,236 @@
 <template>
   <v-app>
     <NavBar />
-    <PopupView :laptops="dataPopup" v-if="isShowPopup" @Close="isShowPopup = false" />
+    <PopupView
+      :laptops="dataPopup"
+      v-if="isShowPopup"
+      @Close="isShowPopup = false"
+    />
     <v-container fluid>
-      <v-card color="#F7F7F7" height="180px" tile flat class="d-flex align-center justify-center mt-12" dark>
+      <v-card
+        color="#F7F7F7"
+        height="180px"
+        tile
+        flat
+        class="d-flex align-center justify-center mt-12"
+        dark
+      >
         <v-col cols="12" sm="12">
           <v-item-group mandatory class="mt-n1">
             <v-container>
               <v-row justify="center" class="space">
-                <v-col cols="12" xs="12" md="2" ms="2" v-for="(category, i) in categories" :key="i">
+                <v-col cols="12" xs="12" md="2" ms="2">
                   <v-item v-slot="{ active }">
-                    <v-card :color="active ? '#D5F0DB' : 'white'" :class="active ? 'borderme' : 'borderout'"
-                      class="d-flex-algin-center rounded-lg mx-2 pa-5" dark height="170"
-                      @click="() => handleCardClick(category.title)" flat>
+                    <v-card
+                      :color="active ? '#D5F0DB' : 'white'"
+                      :class="active ? 'borderme' : 'borderout'"
+                      class="d-flex-algin-center rounded-lg mx-2 pa-5"
+                      dark
+                      height="170"
+                      @click="() => handleCardClick('FPT SHOP')"
+                      flat
+                    >
                       <v-row>
                         <v-col cols="12" sm="12">
-                          <v-list-item three-line class="text-center align-center justify-center pa-0">
+                          <v-list-item
+                            three-line
+                            class="text-center align-center justify-center pa-0"
+                          >
                             <v-list-item-content>
-                              <div align="center" justify="center" class="mt-5" style="height: 70px">
-                                <v-img :src="category.img" max-height="80" max-width="80" contain>
-                                </v-img>
+                              <div
+                                align="center"
+                                justify="center"
+                                class="mt-5"
+                                style="height: 70px"
+                              >
+                                <v-img
+                                  src="../../public/fpt1.png"
+                                  max-height="80"
+                                  max-width="80"
+                                  contain
+                                />
                               </div>
-                              <v-list-item-subtitle :class="active ? 'green--text' : 'black--text'" class="caption mt-5">
-                                {{ category.title }}
+                              <v-list-item-subtitle
+                                :class="active ? 'green--text' : 'black--text'"
+                                class="caption mt-5"
+                              >
+                                FPT SHOP
+                              </v-list-item-subtitle>
+                            </v-list-item-content>
+                          </v-list-item>
+                        </v-col>
+                      </v-row>
+                    </v-card>
+                  </v-item>
+                </v-col>
+                <v-col cols="12" xs="12" md="2" ms="2">
+                  <v-item v-slot="{ active }">
+                    <v-card
+                      :color="active ? '#D5F0DB' : 'white'"
+                      :class="active ? 'borderme' : 'borderout'"
+                      class="d-flex-algin-center rounded-lg mx-2 pa-5"
+                      dark
+                      height="170"
+                      @click="() => handleCardClick('Thế Giới Di Động')"
+                      flat
+                    >
+                      <v-row>
+                        <v-col cols="12" sm="12">
+                          <v-list-item
+                            three-line
+                            class="text-center align-center justify-center pa-0"
+                          >
+                            <v-list-item-content>
+                              <div
+                                align="center"
+                                justify="center"
+                                class="mt-5"
+                                style="height: 70px"
+                              >
+                                <v-img
+                                  src="../../public/tgdd.png"
+                                  max-height="80"
+                                  max-width="80"
+                                  contain
+                                />
+                              </div>
+                              <v-list-item-subtitle
+                                :class="active ? 'green--text' : 'black--text'"
+                                class="caption mt-5"
+                              >
+                                Thế Giới Di Động
+                              </v-list-item-subtitle>
+                            </v-list-item-content>
+                          </v-list-item>
+                        </v-col>
+                      </v-row>
+                    </v-card>
+                  </v-item>
+                </v-col>
+                <v-col cols="12" xs="12" md="2" ms="2">
+                  <v-item v-slot="{ active }">
+                    <v-card
+                      :color="active ? '#D5F0DB' : 'white'"
+                      :class="active ? 'borderme' : 'borderout'"
+                      class="d-flex-algin-center rounded-lg mx-2 pa-5"
+                      dark
+                      height="170"
+                      @click="() => handleCardClick('Phong Vũ')"
+                      flat
+                    >
+                      <v-row>
+                        <v-col cols="12" sm="12">
+                          <v-list-item
+                            three-line
+                            class="text-center align-center justify-center pa-0"
+                          >
+                            <v-list-item-content>
+                              <div
+                                align="center"
+                                justify="center"
+                                class="mt-5"
+                                style="height: 70px"
+                              >
+                                <v-img
+                                  src="../../public/phongvu.png"
+                                  max-height="80"
+                                  max-width="80"
+                                  contain
+                                />
+                              </div>
+                              <v-list-item-subtitle
+                                :class="active ? 'green--text' : 'black--text'"
+                                class="caption mt-5"
+                              >
+                                Phong Vũ
+                              </v-list-item-subtitle>
+                            </v-list-item-content>
+                          </v-list-item>
+                        </v-col>
+                      </v-row>
+                    </v-card>
+                  </v-item>
+                </v-col>
+                <v-col cols="12" xs="12" md="2" ms="2">
+                  <v-item v-slot="{ active }">
+                    <v-card
+                      :color="active ? '#D5F0DB' : 'white'"
+                      :class="active ? 'borderme' : 'borderout'"
+                      class="d-flex-algin-center rounded-lg mx-2 pa-5"
+                      dark
+                      height="170"
+                      @click="() => handleCardClick('HACOM')"
+                      flat
+                    >
+                      <v-row>
+                        <v-col cols="12" sm="12">
+                          <v-list-item
+                            three-line
+                            class="text-center align-center justify-center pa-0"
+                          >
+                            <v-list-item-content>
+                              <div
+                                align="center"
+                                justify="center"
+                                class="mt-5"
+                                style="height: 70px"
+                              >
+                                <v-img
+                                  src="../../public/hacom.png"
+                                  max-height="80"
+                                  max-width="80"
+                                  contain
+                                />
+                              </div>
+                              <v-list-item-subtitle
+                                :class="active ? 'green--text' : 'black--text'"
+                                class="caption mt-5"
+                              >
+                                HACOM
+                              </v-list-item-subtitle>
+                            </v-list-item-content>
+                          </v-list-item>
+                        </v-col>
+                      </v-row>
+                    </v-card>
+                  </v-item>
+                </v-col>
+                <v-col cols="12" xs="12" md="2" ms="2">
+                  <v-item v-slot="{ active }">
+                    <v-card
+                      :color="active ? '#D5F0DB' : 'white'"
+                      :class="active ? 'borderme' : 'borderout'"
+                      class="d-flex-algin-center rounded-lg mx-2 pa-5"
+                      dark
+                      height="170"
+                      @click="() => handleCardClick('Phúc Anh')"
+                      flat
+                    >
+                      <v-row>
+                        <v-col cols="12" sm="12">
+                          <v-list-item
+                            three-line
+                            class="text-center align-center justify-center pa-0"
+                          >
+                            <v-list-item-content>
+                              <div
+                                align="center"
+                                justify="center"
+                                class="mt-5"
+                                style="height: 70px"
+                              >
+                                <v-img
+                                  src="../../public/phucanh.png"
+                                  max-height="80"
+                                  max-width="80"
+                                  contain
+                                />
+                              </div>
+                              <v-list-item-subtitle
+                                :class="active ? 'green--text' : 'black--text'"
+                                class="caption mt-5"
+                              >
+                                Phúc Anh
                               </v-list-item-subtitle>
                             </v-list-item-content>
                           </v-list-item>
@@ -41,14 +249,27 @@
       <v-toolbar color="transparent">
         <v-toolbar-title>Danh sách Laptop</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn varient="text" color="grey">Sort by:</v-btn>
-        <v-btn varient="text" color="">Name</v-btn>
-        <v-btn varient="text" color="grey">Popularity</v-btn>
-        <v-btn varient="text" color="grey">Price</v-btn>
+        <v-text-field
+          density="compact"
+          variant="outlined"
+          label="Search"
+          append-inner-icon="fas fa-search"
+          single-line
+          hide-details
+          class="mr-2"
+          v-model="searchKeyword"
+        ></v-text-field>
       </v-toolbar>
       <v-row class="ma-5">
-        <v-col @click="handleDataPopup(laptop)" cols="12" sm="3" align="center" justify="center"
-          v-for="(laptop, i) in laptops" :key="i">
+        <v-col
+          @click="handleDataPopup(laptop)"
+          cols="12"
+          sm="3"
+          align="center"
+          justify="center"
+          v-for="(laptop, i) in laptops"
+          :key="i"
+        >
           <v-card align="center" tile>
             <v-img :src="laptop.ImageLink" width="200" height="200" contain>
             </v-img>
@@ -56,7 +277,10 @@
               <strong>{{ laptop.Name }}</strong>
             </v-card-text>
             <v-card-text class="">
-              <button class="pa-2 rounded" style="background-color: #cb1c22; color: white">
+              <button
+                class="pa-2 rounded"
+                style="background-color: #cb1c22; color: white"
+              >
                 <strong>{{ laptop.Price }}</strong>
               </button>
             </v-card-text>
@@ -75,10 +299,22 @@
               </div>
             </v-card-text>
             <v-row>
-              <v-col><v-btn class="ma-4" style="background-color: #cb1c22; color: white">
-                  <strong>Mua ngay</strong></v-btn></v-col>
-              <v-col><v-btn class="ma-4" style="background-color: #99a2aa; color: white">
-                  <strong>So sánh</strong></v-btn></v-col>
+              <v-col
+                ><v-btn
+                  class="ma-4"
+                  style="background-color: #cb1c22; color: white"
+                >
+                  <strong>Mua ngay</strong></v-btn
+                ></v-col
+              >
+              <v-col
+                ><v-btn
+                  class="ma-4"
+                  style="background-color: #99a2aa; color: white"
+                >
+                  <strong>So sánh</strong></v-btn
+                ></v-col
+              >
             </v-row>
           </v-card>
         </v-col>
@@ -93,7 +329,9 @@
         </v-btn>
 
         <span class="text-caption"></span>
-        <v-btn rounded @click="rightButton"><v-icon color="grey">fas fa-long-arrow-alt-right</v-icon></v-btn>
+        <v-btn rounded @click="rightButton"
+          ><v-icon color="grey">fas fa-long-arrow-alt-right</v-icon></v-btn
+        >
         <v-spacer></v-spacer>
       </v-toolbar>
     </v-container>
@@ -121,18 +359,23 @@ export default defineComponent({
       other: false,
       slider2: 50,
       categories: [
-        { img: "fpt1.png", title: "FPT SHOP" },
-        { img: "tgdd.png", title: "Thế Giới Di Động" },
-        { img: "phongvu.png", title: "Phong Vũ" },
-        { img: "hacom.png", title: "HACOM" },
-        { img: "phucanh.png", title: "Phúc Anh" },
+        { imgLink: "../../public/fpt1.png", title: "FPT SHOP" },
+        { imgLink: "../../public/tgdd.png", title: "Thế Giới Di Động" },
+        { imgLink: "../../public/phongvu.png", title: "Phong Vũ" },
+        { imgLink: "../../public/hacom.png", title: "HACOM" },
+        { imgLink: "../../public/phucanh.png", title: "Phúc Anh" },
       ],
       laptops: [],
       dataPopup: [],
       isShowPopup: false,
       data: [],
       pageNumber: 1,
+      checkFilter: "",
+      searchKeyword: "",
     };
+  },
+  created() {
+    this.checkFilter = "";
   },
   mounted() {
     this.fetchData();
@@ -143,32 +386,34 @@ export default defineComponent({
       const response = await fetch(fullUrl);
       this.laptops = await response.json();
     },
+    async fetchDataFilter(title) {
+      let fullUrl = `http://localhost:3000/laptops/filter?source=${title}&pageNumber=${this.pageNumber}`;
+      const response = await fetch(fullUrl);
+      this.laptops = await response.json();
+    },
     async handleCardClick(title) {
+      this.pageNumber = 1;
       if (title == "FPT SHOP") {
-        const response = await fetch("http://localhost:3000/laptops/filter?source=fptshop");
-        this.laptops = await response.json();
-
+        this.checkFilter = "fptshop";
+        this.fetchDataFilter(this.checkFilter);
       }
       if (title == "Thế Giới Di Động") {
-        const response = await fetch("http://localhost:3000/laptops/filter?source=tgdd");
-        this.laptops = await response.json();
-
+        this.checkFilter = "tgdd";
+        this.fetchDataFilter(this.checkFilter);
       }
       if (title == "Phong Vũ") {
-        const response = await fetch("http://localhost:3000/laptops/filter?source=phongvu");
-        this.laptops = await response.json();
-
+        this.checkFilter = "phongvu";
+        this.fetchDataFilter(this.checkFilter);
       }
       if (title == "HACOM") {
-        const response = await fetch("http://localhost:3000/laptops/filter?source=hanoicomputer");
-        this.laptops = await response.json();
-
+        this.checkFilter = "hanoicomputer";
+        this.fetchDataFilter(this.checkFilter);
       }
       if (title == "Phúc Anh") {
-        const response = await fetch("http://localhost:3000/laptops/filter?source=phucanh");
-        this.laptops = await response.json();
-
+        this.checkFilter = "phucanh";
+        this.fetchDataFilter(this.checkFilter);
       }
+      console.log(this.laptops);
     },
     handleDataPopup(laptop) {
       this.dataPopup = laptop.matching;
@@ -180,10 +425,12 @@ export default defineComponent({
       } else {
         this.pageNumber = 1;
       }
+
       this.fetchData();
     },
     rightButton() {
       this.pageNumber++;
+
       this.fetchData();
     },
   },
@@ -195,6 +442,18 @@ export default defineComponent({
         }
       },
     },
+    searchKeyword: {
+      handler: function (newValue) {
+        if (newValue) {
+          setTimeout(async () => {
+            let fullUrl = `http://localhost:3000/laptops/search?name=${newValue}&pageNumber=1`;
+            const response = await fetch(fullUrl);
+            this.laptops = await response.json();
+          }, 500);
+        }
+      },
+    },
+    
   },
 });
 </script>
